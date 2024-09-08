ed76f49a82426f07596feb1192ca6807b990f80b
robo2020 <robo2020@gmail.com>
2024-09-08 08:15:46 +0000

    mapping joystick types

--- src/joystick_client/joystick_client/main.py
+++ src/joystick_client/joystick_client/main.py
@@ -1,16 +1,20 @@
 # from rome_client.utils import Singleton
-import zenoh
-import logging
-from typing import List
-import time
-from threading import Thread
-from linux_joystick import AxisEvent, ButtonEvent, Joystick as JoySimple
-from pycdr2 import IdlStruct
-from pycdr2.types import int8, int32, uint32, float64, float32, uint8, array
-from dataclasses import dataclass
-from joystick_interface.msg import Joystick 
 import argparse
+import json
 import logging
+import os
+import time
+from dataclasses import dataclass
+from threading import Thread
+from typing import List
+
+import zenoh
+from linux_joystick import AxisEvent, ButtonEvent
+from linux_joystick import Joystick as JoySimple
+from pycdr2 import IdlStruct
+from pycdr2.types import array, float32, float64, int8, int32, uint8, uint32
+
+from joystick_interface.msg import Joystick
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -21,7 +25,8 @@ class JoystickData(IdlStruct, typename="JoystickData"):
     axes: List[int8]
     buttons: List[int8]
 
-class ZenohBackend():
+
+class ZenohBackend:
     def __init__(self, ip, port) -> None:
         self.cfg = zenoh.Config.from_obj(self.build_config(ip, port))
         self.__session = None
@@ -30,12 +35,10 @@ class ZenohBackend():
         log.info(f"Try connect to server: {ip}:{port}")
         config_dict = {
             "mode": "client",
-            "connect": {
-                "endpoints": ["udp/{}:{}".format(ip, port)]
-            }
+            "connect": {"endpoints": ["udp/{}:{}".format(ip, port)]},
         }
         return config_dict
-    
+
     def open(self):
         while True:
             try:
@@ -47,22 +50,38 @@ class ZenohBackend():
                 log.error("zenoh session open failed try again", exc_info=True)
                 time.sleep(2)
 
-
     def pub(self, joy_state: JoystickData):
         self.__session.put("joystick", joy_state.serialize())
-
 
     @property
     def session(self):
         return self.__session
-    
 
-class JoystickManager():
-    def __init__(self, cb):
-        self.joy_state = JoystickData(axes=[0]*8, buttons=[0]*16)
-        self.notify_thread = Thread(target=self.__notify, daemon=True, name="joy_notify")
+
+class JoystickManager:
+    def __init__(self, cb, mapping):
+        self.joy_state = JoystickData(axes=[0] * 8, buttons=[0] * 16)
+        self.notify_thread = Thread(
+            target=self.__notify, daemon=True, name="joy_notify"
+        )
         self.notify_thread.start()
         self.notify = cb
+        self.__mapping = self.__load_mapping(mapping)
+
+    def __load_mapping(self, mapping):
+        file_path = os.path.dirname(__file__)
+        file_path = os.path.join(file_path, mapping + ".json")
+        if not os.path.exists(file_path):
+            log.error(f"joystick mapping not found: {mapping}")
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        return data
+
+    def __resolve_mapping(self, type, id):
+        id = str(id)
+        if id not in self.__mapping[type]:
+            raise Exception("Mapping not found")
+        return self.__mapping[type][id]
 
     def map_axis(self, axis_value):
         value = axis_value / AxisEvent.MAX_AXIS_VALUE * Joystick.AXIS_MAX
@@ -70,7 +89,7 @@ class JoystickManager():
 
     def __notify(self):
         while True:
-            time.sleep(1/10)
+            time.sleep(1 / 10)
             self.notify(self.joy_state)
 
     def run(self):
@@ -81,28 +100,43 @@ class JoystickManager():
                     event = js.poll()
                     if isinstance(event, AxisEvent):
                         value = self.map_axis(event.value)
-                        self.joy_state.axes[event.id] = value
+                        id = self.__resolve_mapping("axes", event.id)
+                        self.joy_state.axes[id] = value
                         # print(f"Axis {event.id} is {event.value}")
                     elif isinstance(event, ButtonEvent):
-                        self.joy_state.buttons[event.id] = int(event.value)
+                        id = self.__resolve_mapping("buttons", event.id)
+                        self.joy_state.buttons[id] = int(event.value)
+                        print(id, int(event.value))
                         # print(f"Button {event.id} is {event.value}")
             except Exception as err:
                 log.error(err)
                 log.error("no device")
-                time.sleep(1/2)
+                time.sleep(1 / 2)
+
 
 if __name__ == "__main__":
     log.info("simple joy :)")
-    parser=argparse.ArgumentParser(description="joy client zenoh connect settings")
-    parser.add_argument("--server_ip", default="172.17.0.1", type=str, help="server address", required=False)
-    parser.add_argument("--port", default=35407, type=int, help="server port", required=False)
+    parser = argparse.ArgumentParser(description="joy client zenoh connect settings")
+    parser.add_argument(
+        "--server_ip",
+        default="172.17.0.1",
+        type=str,
+        help="server address",
+        required=False,
+    )
+    parser.add_argument(
+        "--port", default=35407, type=int, help="server port", required=False
+    )
+    parser.add_argument(
+        "--joy_type", default="logitech-xbox", help="logitech-xbox, logitech-extreme-3d"
+    )
     args = parser.parse_args()
 
     z = ZenohBackend(args.server_ip, args.port)
     z.open()
-    joy_manager = JoystickManager(cb=z.pub)
+    joy_manager = JoystickManager(cb=z.pub, mapping=args.joy_type)
     joy_manager.run()
-    
+
     # p = z.create_publisher("joystick")
     # buf = Joystick([0]*8, [0]*8)
     # counter = 1
